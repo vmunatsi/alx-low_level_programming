6700f73bd0ed7bf936a4c3eb485652e9051c69fe
Vincent <vincentm43@gmail.com>
2022-06-13 11:57:48 -0700

    task 6

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,32 +6,20 @@
  *@s: table of caracter
  *Return: Void
  */
-
 void rev_string(char *s)
 {
-	long int i = sizeof(s);
-	char *p = s;
-	/* while (*p != '\0') */
-	/* { */
-	/* 	printf("%c" ,*p++); */
-	/*  } */
-	printf("lent of p = %ld\n", sizeof(p));
+	char rev = s[0];
+	int fcounter = 0;
+	int i;
 
-	while (i >= 0)
+	while (s[fcounter] != '\0')
+		fcounter++;
+
+	for (i = 0; i < fcounter; i++)
 	{
-		/* printf("i=%d\n",i); */
-		/* printf("siz-i=%ld\n",sizeof(s) - i); */
-		/* printf ("%c", s[i--]); */
-		*(p + (sizeof(s) - i)) = s[i];
-		/* printf("sizeof = %ld \n", sizeof(s)); */
-		/* printf("%c\n", s[i]); */
-		/* p++; */
-		printf("\np=%c\n" ,*p);
-	       printf("size of p = %ld\n", sizeof(p));
-		i--;
-
+		fcounter--;
+		rev = s[i];
+		s[i] = s[fcounter];
+		s[fcounter] = rev;
 	}
-	printf("\n");
-	/* sleep(2); */
-	/* *s = *p; */
 }
