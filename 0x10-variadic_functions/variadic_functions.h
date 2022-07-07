dc52137cf6f9413450d501a67e1f6db248b15203
Vincent <vincentm43@gmail.com>
2022-07-07 11:33:23 -0700

    Added variadic function library

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,14 +1,10 @@
-#ifndef VARIADIC_H
-#define VARIADIC_H
-#include <stdarg.h>
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _putchar(char c);
+void print_strings(const char *separator, const unsigned int n, ...);
 
-void print_int(va_list list);
-void print_float(va_list list);
-void print_char(va_list list);
-void print_str(va_list list);
+#endif /* VARIADIC_FUNCTIONS_H */
