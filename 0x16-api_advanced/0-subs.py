7939186a49c4580d4f7622fa89c397515e153d0a
vincent <vincentm43@gmail.com>
2023-01-31 10:45:10 +0000

    error fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-""" module 0 """
-from requests import get
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """ number_of_subscribers - Returns the number of subscribers for
-        a given subreddit.
-        parameters: subreddit.
-        Return: the numeber of subscribers or 0 is not a valid subreddit.
-    """
-    request = get('https://www.reddit.com/r/{}/about.json'
-                  .format(subreddit), headers={'User-Agent': 'jdarangop'})
-
-    if request.status_code != 200:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    else:
-        request_json = request.json()
-        return request_json.get('data').get('subscribers')
+    results = response.json().get("data")
+    return results.get("subscribers")
